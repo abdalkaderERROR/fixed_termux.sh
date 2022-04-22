91fa91e881b4af1eeaa82cebe980c2c71c16fc70
abdalkaderERROR <84675967+abdalkaderERROR@users.noreply.github.com>
2022-04-22 22:40:46 +0300

    txt.tx
    you cant install 
    git clone

---
+++ fixed_termux.sh
@@ -0,0 +1,32 @@
+echo "email : abdalkaderzx54@gmail.com"
+echo " "
+echo " "
+
+sleep 1.5
+
+echo "Welcome to my tools !"
+
+echo " you are kids hack ?!"
+
+echo "y or n"
+
+read think
+
+echo "remov the pkg error plaiz whait the chick:!"
+
+pkg remove game-repo
+
+echo "this true !"
+
+
+pkg remove science-repo
+
+echo " True :"
+
+echo "____________________________"
+
+pkg update -y
+
+echo "____________________________"
+
+pkg upgrade -y
